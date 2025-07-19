c0688eba7094bdc8e0d7980564756b482e3fd334
Bhaumik Solanki <bhaumiksolanki.dev@gmail.com>
2025-07-20 00:29:58 +0530

    Update send-email.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch("/.netlify/functions/send-email", {
+      const response = await fetch("../../netlify/functions/send-email.js", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
